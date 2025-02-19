0582c1a835390f6d4609c2815ede00d495774f53
EvanM222 <Bayleemusick.22@gmail.com>
2025-02-19 14:50:14 -0600

    added fun fact generator

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById('project1').onclick = function() {
-   
+const funFacts = [
+    'Southwest MO is comprised of seven counties, with Joplin being the region’s largest city.',
+    'Southwest MO is home to the Ozark Mountains, which are known for their beauty and outdoor recreational opportunities.',
+    'Southwest MO is known for its rich history, including the Trail of Tears, which passed through the region in the 1830s.',
+    'Southwest Mo is home to a number of state parks, including Roaring River State Park, which is known for its trout fishing.',
+ ]
+
+
+let previousIndex = -1;
+
+document.getElementById('fact-button').onclick = function() {
+   for (let i = 0; i < funFacts.length; i++) {
+    do{
+        randomIndex = Math.floor(Math.random() * funFacts.length);
+    }while(randomIndex === previousIndex);
+    document.getElementById('fun-fact').innerHTML = funFacts[randomIndex];
+    previousIndex = randomIndex;
+    }
 };
